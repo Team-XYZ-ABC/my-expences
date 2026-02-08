5868cec8dc0ef55317faedc3a8f0a041ae5de21a
Md Alkama <mdalkamadad@gmail.com>
2026-02-08 15:09:23 +0530

    chore: initial project setup

---
+++ client/src/App.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import Button from '@components/buttons/Button'
+import { IoIosSearch } from "react-icons/io";
+import { MdSaveAlt } from "react-icons/md";
+import FormLayout from '@components/layouts/FormLayout';
+import InputBox from './components/inputs/InputBox';
+import Input from './components/inputs/Input';
+import { FaGoogle } from "react-icons/fa";
+import { FaApple } from "react-icons/fa";
+import { FaArrowRight } from "react-icons/fa6";
+import Breaker from './components/Extras/Breaker';
+import TitleDescription from '@components/texts/TitleDescription';
+import Link from '@components/links/Link'
+import Description from './components/texts/Description';
+
+
+
+
+
+
+
+const App = () => {
+
+  function first() {
+    console.log("save image")
+  }
+  function second() {
+    console.log("second")
+  }
+  function third() {
+    console.log("third")
+  }
+
+  return (
+    <div>
+      App
+    </div>
+  )
+}
+
+export default App
