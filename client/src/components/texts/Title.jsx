0f06e0e0934fb6ca9f870882923f712fb5d104df
Md Alkama <mdalkamadad@gmail.com>
2026-02-08 15:15:21 +0530

    feat(ui): add reusable title component

---
+++ client/src/components/texts/Title.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Title = ({title}) => {
+  return (
+    <div>
+          <p className='text-4xl text-start font-semibold select-none'>{title}</p>
+    </div>
+  )
+}
+
+export default Title
