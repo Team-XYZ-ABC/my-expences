fda8d8a3c8776a10dad1e9e54ebdc1adaf4832a2
Md Alkama <mdalkamadad@gmail.com>
2026-02-08 15:15:15 +0530

    feat(ui): add reusable description component

---
+++ client/src/components/texts/Description.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Description = ({description}) => {
+  return (
+      <p className='text-sm text-gray-500 text-start select-none'>{description}</p>
+  )
+}
+
+export default Description
