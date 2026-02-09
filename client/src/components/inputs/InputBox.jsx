055f1fa273269cfc526485e58e4660b783a34d7f
Md Alkama <mdalkamadad@gmail.com>
2026-02-09 17:40:41 +0530

    fix(font): fix font of label

--- client/src/components/inputs/InputBox.jsx
+++ client/src/components/inputs/InputBox.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const InputBox = ({type, label, placeholder, children}) => {
     return (
         <div className='w-full py-2.5'>
-            <p className='py-1'>{label}</p>
+            <p className='py-1 font-medium'>{label}</p>
             {children}
         </div>
     )
