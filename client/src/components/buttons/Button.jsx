1a2d40a17662614ff0547c82839bb812750ec1f7
Md Alkama <mdalkamadad@gmail.com>
2026-02-09 17:52:48 +0530

    fix(button): changes in ui and some logics

--- client/src/components/buttons/Button.jsx
+++ client/src/components/buttons/Button.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
-const Button = ({SIcon, LIcon, label, btnColor = "#092640", textColor = "black", action = () => { }}) => {
-
+const Button = ({
+    SIcon,
+    LIcon,
+    label,
+    btnColor = "#092640",
+    textColor = "black",
+    action = () => { },
+}) => {
     return (
         <button
             onClick={() => {
@@ -11,13 +17,13 @@ const Button = ({SIcon, LIcon, label, btnColor = "#092640", textColor = "black",
                 backgroundColor: btnColor,
                 color: textColor,
             }}
-            className={`bg-[#092640] hover:opacity-90 active:scale-99 w-full flex my-4 justify-center items-center rounded-md text-lg py-3.5 
-                ${(LIcon || SIcon) && !label ? "aspect-square" : "px-10"} 
-                ${btnColor == 'transparent' ? "border border-gray-300" : null}`}
+            className={`bg-[#092640] hover:opacity-90 active:scale-99 text-nowrap  flex my-4 justify-center items-center rounded-md text-lg py-3.5 
+                ${(LIcon || SIcon) && !label ? "aspect-square" : "px-10 w-full"} 
+                ${btnColor == "transparent" ? "border border-gray-300" : null}`}
         >
-            {SIcon && <SIcon className="mr-2.5"/>}
+            {SIcon && <SIcon className="mx-2.5" />}
             {label && label}
-            {LIcon && <LIcon className="ml-5"/>}
+            {LIcon && <LIcon className="mx-5" />}
         </button>
     );
 };
