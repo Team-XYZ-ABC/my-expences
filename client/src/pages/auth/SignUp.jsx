a779c5fd3d6b7778ae5c548c1fab03eaacb5d145
Md Alkama <mdalkamadad@gmail.com>
2026-02-10 06:30:22 +0530

    fix(signup): fixes layout and responsiveness

--- client/src/pages/auth/SignUp.jsx
+++ client/src/pages/auth/SignUp.jsx
@@ -1,14 +1,14 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import Title from "@components/texts/Title";
 import FormLayout from "@components/layouts/FormLayout";
 import Description from "@components/texts/Description";
 import Button from "@/components/buttons/Button";
 import { FcGoogle } from "react-icons/fc";
-import { FaApple } from "react-icons/fa";
 import Breaker from "@/components/Extras/Breaker";
 import Input from "@/components/inputs/Input";
 import InputBox from "@/components/inputs/InputBox";
 import Link from "@/components/links/Link";
+import TitleDescription from "@/components/texts/TitleDescription";
 
 const SignUp = () => {
 
@@ -17,21 +17,13 @@ const SignUp = () => {
   const [password, setPassword] = useState("");
 
   return (
-    <div className="w-full min-h-[calc(100vh-60px)] flex  justify-between">
+    <div className="w-full pt-20 min-h-screen flex  justify-between">
       <div className="w-1/2 bg-blue-500 hidden lg:flex"></div>
       <div className="w-full my-5 flex justify-center items-center lg:w-1/2 bg-white p-5">
         <FormLayout>
-          <div className="flex flex-col gap-4 ">
-            <Title>Create your account</Title>
-            <Description
-              font="m"
-              description={
-                "Track your expenses, stay organized, and manage your money better."
-              }
-            />
-          </div>
 
-
+          <TitleDescription title={"Create Account"} description={"Track your expenses, stay organized, and manage your money better."} />
+          
           <InputBox label={"Full Name"}>
             <Input placeholder={"Enter your name"} value={fullName} onChange={(e)=>{
               setFullName(e.target.value)
@@ -50,7 +42,7 @@ const SignUp = () => {
             }} type={"password"} />
           </InputBox>
 
-          <div className="my-10 flex items-center flex-wrap">
+          <div className="my-5 flex items-center flex-wrap">
             <input
               className="h-4 w-4 mr-2.5 accent-blue-500"
               type="checkbox"
@@ -64,13 +56,14 @@ const SignUp = () => {
 
           <Button label={"Creat Account"} textColor={"white"} />
 
-          <Breaker label={"OR CONTINUE WITH"} />
+          {/* <Breaker label={"OR CONTINUE WITH"} /> */}
           <Button SIcon={FcGoogle} label={"Sign in with Google"} btnColor={"transparent"} />
-
           <div className="flex w-full justify-center">
             <Description description="Already have an account?" />
             <Link label="Sign in" />
           </div>
+
+
         </FormLayout>
       </div>
 
