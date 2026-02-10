2f0eae798849a5245172a0555d98dfc9144fb108
umair khan <ukkhan5476@gmail.com>
2026-02-10 06:18:53 +0530

    feat(navbar): add basic navbar(no responsive added)

---
+++ client/src/components/Navigations/Navbar.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { MdOutlineAccountBalanceWallet } from "react-icons/md";
+import Button from "../buttons/Button";
+
+const Navbar = () => {
+  return (
+    <div className="w-full h-20.25 bg-white flex items-center justify-between p-4 shadow-lg fixed ">
+      <div className="flex items-center gap-10">
+        <div className="flex items-center ">
+          <MdOutlineAccountBalanceWallet className="w-10 h-10 text-black p-2 rounded-md " />
+          <span className="text-2xl font-extrabold text-black">
+            FinanceTracker
+          </span>
+        </div>
+        <div className="flex items-center gap-4 font-semibold text-gray-600 ">
+          <a
+            className="hover:underline underline-offset-4 decoration-2"
+            href="#"
+          >
+            Dashboard
+          </a>
+          <a
+            className="hover:underline underline-offset-4 decoration-2"
+            href="#"
+          >
+            Analytics
+          </a>
+          <a
+            className="hover:underline underline-offset-4 decoration-2"
+            href="#"
+          >
+            Budgets
+          </a>
+          <a
+            className="hover:underline underline-offset-4 decoration-2"
+            href="#"
+          >
+            Transactions
+          </a>
+        </div>
+      </div>
+      <div className="flex items-center gap-5 text-black font-semibold">
+        <Button label={"Sign In"} btnColor={"transparent"} />
+        <Button label={"Sign up"} btnColor={"black"} textColor="white" />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
